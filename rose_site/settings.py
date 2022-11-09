78a58f6c32e88fcec753f89f5e1fc482dcebeab9
jacky <a.bojic22@gmail.com>
2022-11-09 14:09:11 +0000

    Remove old staf

--- rose_site/settings.py
+++ rose_site/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-o-w@64wd(-q_xzkj)tf^zvy5yi^&7$f-z(ijwwuy%l5fp9s@n#'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['jacky2256.pythonanywhere.com']
 
 
 # Application definition
@@ -120,6 +120,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 MEDIA_URL = '/media/'
 
